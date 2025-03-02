9ac07a17393c4710ada29cdba8786aa945d3d6e4
Jesus Bocanegra <jojadev@proton.me>
2025-03-01 19:27:36 -0600

    Fixed Lint Issues in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig, type PluginOption } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
+import process from "node:process";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss() as any, sveltekit()],
+  plugins: [tailwindcss() as PluginOption, sveltekit()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
